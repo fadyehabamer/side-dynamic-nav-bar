8361e7ab7434c4c4d233b84466b4b78f70ae57d7
fadyehabamer <fadyamer45@gmail.com>
2021-02-14 04:33:03 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+var sections = document.querySelectorAll("section");
+
+onscroll = function () {
+  var scrollPosition = document.documentElement.scrollTop;
+
+  sections.forEach((section) => {
+    if (
+      scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
+      scrollPosition <
+        section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25
+    ) {
+      var currentId = section.attributes.id.value;
+      removeAllActiveClasses();
+      addActiveClass(currentId);
+    }
+  });
+};
+
+var removeAllActiveClasses = function () {
+  document.querySelectorAll("nav a").forEach((el) => {
+    el.classList.remove("active");
+  });
+};
+
+var addActiveClass = function (id) {
+  // console.log(id);
+  var selector = `nav a[href="#${id}"]`;
+  document.querySelector(selector).classList.add("active");
+};
+
+var navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    var currentId = e.target.attributes.href.value;
+    var section = document.querySelector(currentId);
+    var sectionPos = section.offsetTop;
+    // section.scrollIntoView({
+    //   behavior: "smooth",
+    // });
+
+    window.scroll({
+      top: sectionPos,
+      behavior: "smooth",
+    });
+  });
+});
